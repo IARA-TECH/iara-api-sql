97c17b829a21b9bb51994470e7289faeef4785bc
melpiress <melissa.foneca@germinare.org.br>
2025-08-22 10:49:58 -0300

    feat: add UserPhoto class

--- src/main/java/com/iaraapi/model/UserPhoto.java
+++ src/main/java/com/iaraapi/model/UserPhoto.java
@@ -1,14 +1,35 @@
 package com.iaraapi.model;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_account_photo")
 public class UserPhoto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    private Integer createdAt
+    @NotBlank(message = "Url blob is required.")
+    @Size(max = 100, message = "Url blob has a maximum of 100 characters.")
+    private String urlBlob;
 
+    @ManyToOne
+    @JoinColumn(name = "user_account_uuid")
+    @NotNull(message = "User is required.")
+    private User user;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(insertable = false)
+    private LocalDateTime changedAt;
 }
