9e721fb093fd2f76f7954015c334bbc3e35b38ce
melpiress <157593096+melpiress@users.noreply.github.com>
2025-08-26 11:32:18 -0300

    Delete src/main/java/com/iaraapi/repository/FieldRepository.java

--- src/main/java/com/iaraapi/repository/FieldRepository.java
+++
@@ -1,7 +0,0 @@
-package com.iaraapi.repository;
-
-import com.iaraapi.model.Field;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FieldRepository extends JpaRepository<Field, Long> {
-}
