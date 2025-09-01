50e0890cec8fc538d8bf8a49595c60e626435bf8
melpiress <melissa.foneca@germinare.org.br>
2025-09-01 08:26:42 -0300

    feat: add Exception Handler and InvalidDataException

---
+++ src/main/java/com/iaraapi/exception/InvalidDataException.java
@@ -0,0 +1,15 @@
+package com.iaraapi.exception;
+
+import lombok.Getter;
+
+import java.util.Map;
+
+@Getter
+public class InvalidDataException extends RuntimeException {
+    private final Map<String, String> errors;
+    public InvalidDataException(Map<String, String> errors) {
+        super("Invalid data.");
+        this.errors = errors;
+    }
+
+}
