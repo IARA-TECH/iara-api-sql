c3e097840d84251eb0f1f8b641d1c4fd342a04d2
melpiress <melissa.foneca@germinare.org.br>
2025-09-05 12:15:40 -0300

    feat: add PaymentMethod request and response dto

---
+++ src/main/java/com/iaraapi/dto/request/PaymentMethodRequest.java
@@ -0,0 +1,11 @@
+package com.iaraapi.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class PaymentMethodRequest {
+    @NotBlank(message = "Name is required")
+    private String name;
+}
---
+++ src/main/java/com/iaraapi/dto/response/PaymentMethodResponse.java
@@ -0,0 +1,12 @@
+package com.iaraapi.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PaymentMethodResponse {
+    private String name;
+}
