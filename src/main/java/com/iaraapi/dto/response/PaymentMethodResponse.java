14de4607560730274f8c972f2b3c6b63a313eed6
melpiress <melissa.foneca@germinare.org.br>
2025-09-06 18:54:23 -0300

    fix: change lombok annotations to PaymentMethodResponse

--- src/main/java/com/iaraapi/dto/response/PaymentMethodResponse.java
+++ src/main/java/com/iaraapi/dto/response/PaymentMethodResponse.java
@@ -2,11 +2,11 @@ package com.iaraapi.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @AllArgsConstructor
-@NoArgsConstructor
 public class PaymentMethodResponse {
-    private String name;
+    private final String name;
 }
