349c82e9c14b52c1b29847fa170b245548a563f4
melpiress <melissa.foneca@germinare.org.br>
2025-09-06 18:48:21 -0300

    fix: adding lombok annotations to PaymentMethodRequest

--- src/main/java/com/iaraapi/dto/request/PaymentMethodRequest.java
+++ src/main/java/com/iaraapi/dto/request/PaymentMethodRequest.java
@@ -3,7 +3,13 @@ package com.iaraapi.dto.request;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class PaymentMethodRequest {
     @NotBlank(message = "Name is required")
