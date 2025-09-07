bfe6ad3e728584e53d03a42f515a8758eee421c7
melpiress <melissa.foneca@germinare.org.br>
2025-09-07 18:20:51 -0300

    fix: removing unusaged imports and changing annotation in subscription response

--- src/main/java/com/iaraapi/dto/response/SubscriptionResponse.java
+++ src/main/java/com/iaraapi/dto/response/SubscriptionResponse.java
@@ -1,11 +1,10 @@
 package com.iaraapi.dto.response;
 
-import jakarta.persistence.Column;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Data
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
 public class SubscriptionResponse {
