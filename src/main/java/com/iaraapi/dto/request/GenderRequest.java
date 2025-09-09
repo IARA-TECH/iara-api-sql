3c0e0d8ccbd56e13c814da75878462da4972c94f
melpiress <melissa.foneca@germinare.org.br>
2025-09-09 08:01:30 -0300

    feat: add gender request dto

---
+++ src/main/java/com/iaraapi/dto/request/GenderRequest.java
@@ -0,0 +1,19 @@
+package com.iaraapi.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class GenderRequest {
+    @NotBlank(message = "Name is required.")
+    @Size(max = 20, message = "Name must have a maximum of 20 characters")
+    private String name;
+}
