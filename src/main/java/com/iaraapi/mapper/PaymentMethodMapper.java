ff371b03f86b49ee6c61645a1b9921d5e49ffb16
melpiress <melissa.foneca@germinare.org.br>
2025-09-15 10:49:44 -0300

    feat: add payment method service

--- src/main/java/com/iaraapi/mapper/PaymentMethodMapper.java
+++ src/main/java/com/iaraapi/mapper/PaymentMethodMapper.java
@@ -1,5 +1,6 @@
 package com.iaraapi.mapper;
 
+import com.iaraapi.dto.request.PaymentMethodRequest;
 import com.iaraapi.dto.response.PaymentMethodResponse;
 import com.iaraapi.model.PaymentMethod;
 import org.mapstruct.Mapper;
@@ -8,7 +9,7 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface PaymentMethodMapper {
     @Mapping(target = "id", ignore = true)
-    PaymentMethod toEntity(PaymentMethod paymentMethod);
+    PaymentMethod toEntity(PaymentMethodRequest paymentMethodRequest);
 
     PaymentMethodResponse toResponse(PaymentMethod paymentMethod);
 }
