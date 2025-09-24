36feb626b2bc69db7fabc725d36de29a2a168f6e
melpiress <melissa.foneca@germinare.org.br>
2025-09-23 22:46:49 -0300

    feat: fix user photo entity after update on database

---
+++ src/main/java/com/iaraapi/dto/request/UserPhotoRequest.java
@@ -0,0 +1,20 @@
+package com.iaraapi.dto.request;
+
+import com.iaraapi.model.User;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.UUID;
+
+public class UserPhotoRequest {
+    @NotBlank(message = "Url blob is required.")
+    @Size(max = 100, message = "Url blob has a maximum of 100 characters.")
+    private String urlBlob;
+
+    @NotNull(message = "User is required.")
+    private UUID userId;
+
+}
--- src/main/java/com/iaraapi/model/User.java
+++ src/main/java/com/iaraapi/model/User.java
@@ -1,6 +1,12 @@
 package com.iaraapi.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Data;
