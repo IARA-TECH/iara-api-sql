02a2ea0995f9f74966b21cfcf8535cdce5c01728
melpiress <melissa.foneca@germinare.org.br>
2025-10-04 16:23:39 -0300

    feat: add user photo mapper

---
+++ src/main/java/com/iaraapi/mapper/UserPhotoMapper.java
@@ -0,0 +1,18 @@
+package com.iaraapi.mapper;
+
+import com.iaraapi.dto.request.UserPhotoRequest;
+import com.iaraapi.dto.response.UserPhotoResponse;
+import com.iaraapi.model.User;
+import com.iaraapi.model.UserPhoto;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface UserPhotoMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(source = "user.id", target = "user.id")
+    UserPhoto toEntity(UserPhotoRequest request, User user);
+
+    @Mapping(source = "user.id", target = "userId")
+    UserPhotoResponse toResponse(UserPhoto userPhoto);
+}
