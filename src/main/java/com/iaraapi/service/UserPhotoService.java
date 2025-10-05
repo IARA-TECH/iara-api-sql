403f95b10baf8adda5678791f8c7265a06190bb8
melpiress <melissa.foneca@germinare.org.br>
2025-10-05 19:59:28 -0300

    fix: adjust create method to call procedure in database

--- src/main/java/com/iaraapi/service/UserPhotoService.java
+++ src/main/java/com/iaraapi/service/UserPhotoService.java
@@ -8,20 +8,25 @@ import com.iaraapi.model.UserPhoto;
 import com.iaraapi.repository.UserPhotoRepository;
 import com.iaraapi.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 
 @Service
+@Slf4j
 public class UserPhotoService extends BaseService<UserPhoto, Long, UserPhotoRequest, UserPhotoResponse> {
     private final UserPhotoMapper mapper;
     private final UserRepository userRepository;
+    private final UserPhotoRepository userPhotoRepository;
 
-    public UserPhotoService(UserPhotoRepository repository, UserPhotoMapper mapper, UserRepository userRepository) {
+    public UserPhotoService(UserPhotoRepository repository, UserPhotoMapper mapper,
+                            UserRepository userRepository, UserPhotoRepository userPhotoRepository) {
         super(repository, "UserPhoto");
         this.mapper = mapper;
         this.userRepository = userRepository;
+        this.userPhotoRepository = userPhotoRepository;
     }
 
 
@@ -30,6 +35,22 @@ public class UserPhotoService extends BaseService<UserPhoto, Long, UserPhotoRequ
         User user = userRepository.findById(request.getUserId())
                 .orElseThrow(() -> new EntityNotFoundException("User with ID " + request.getUserId() + " not found"));
         return mapper.toEntity(request, user);
+    }
+
+    @Override
+    public UserPhotoResponse create(UserPhotoRequest request) {
+        log.info("[UserAccountPhotoService] Creating photo for user {}", request.getUserId());
+        try {
+            userPhotoRepository.createUserAccountPhoto(request.getUserId(), request.getUrlBlob());
+            log.info("[UserAccountPhotoService] Photo created successfully for user {}", request.getUserId());
+            return new UserPhotoResponse(
+                    request.getUrlBlob(),
+                    request.getUserId()
+            );
+        } catch (Exception e) {
+            log.error("[UserAccountPhotoService] Error creating user photo: {}", e.getMessage(), e);
+            throw new RuntimeException("Error creating user photo.");
+        }
     }
 
     @Override
