7983f9411a1dd8125f75065c5cc65b8e77bf2c52
melpiress <melissa.foneca@germinare.org.br>
2025-10-06 16:03:38 -0300

    feat: add user access type service

--- src/main/java/com/iaraapi/dto/request/UserAccessTypeRequest.java
+++ src/main/java/com/iaraapi/dto/request/UserAccessTypeRequest.java
@@ -2,11 +2,17 @@ package com.iaraapi.dto.request;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+
+import java.util.UUID;
 
 @Getter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class UserAccessTypeRequest {
+    @NotNull(message = "User ID is required.")
     private Integer accessTypeId;
-    private Integer userId;
+
+    @NotNull(message = "Access Type ID is required.")
+    private UUID userId;
 }
