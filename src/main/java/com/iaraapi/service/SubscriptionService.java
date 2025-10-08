65034baaa4b388409eaadd0f9fd7fed3cbf5ce30
melpiress <melissa.foneca@germinare.org.br>
2025-10-08 10:08:04 -0300

    refactor: add transactional annotation to SubscriptionService

--- src/main/java/com/iaraapi/service/SubscriptionService.java
+++ src/main/java/com/iaraapi/service/SubscriptionService.java
@@ -6,6 +6,7 @@ import com.iaraapi.model.Subscription;
 import com.iaraapi.dto.request.SubscriptionRequest;
 import com.iaraapi.repository.SubscriptionRepository;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -44,6 +45,7 @@ public class SubscriptionService extends BaseService<Subscription, Integer, Subs
     }
 
     @Override
+    @Transactional
     public SubscriptionResponse deactivateEntity(Integer id) {
         log.info("[SubscriptionService] [deactivateEntity] DEACTIVATE SUBSCRIPTION WITH ID {}", id);
         Subscription subscription = repository.findById(id)
@@ -55,6 +57,7 @@ public class SubscriptionService extends BaseService<Subscription, Integer, Subs
     }
 
     @Override
+    @Transactional
     public SubscriptionResponse reactivateEntity(Integer id) {
         log.info("[SubscriptionService] [reactivateEntity] REACTIVATE SUBSCRIPTION WITH ID {}", id);
         Subscription subscription = repository.findById(id)
