6142f60458d79bd498ee5f990d992c7968316178
melpiress <melissa.foneca@germinare.org.br>
2025-10-16 07:11:01 -0300

    fix: add column name to id

--- src/main/java/com/iaraapi/model/database/DailyActiveUsers.java
+++ src/main/java/com/iaraapi/model/database/DailyActiveUsers.java
@@ -1,12 +1,6 @@
 package com.iaraapi.model.database;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +16,7 @@ import java.time.LocalDateTime;
 public class DailyActiveUsers {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "pk_id")
     private Integer id;
 
     @CreationTimestamp
