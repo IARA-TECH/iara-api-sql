8ac1b66758ae6a41facfed86144fe1159a5156d8
melpiress <melissa.foneca@germinare.org.br>
2025-10-16 07:14:01 -0300

    fix: remove unused import

--- src/main/java/com/iaraapi/repository/UserPhotoRepository.java
+++ src/main/java/com/iaraapi/repository/UserPhotoRepository.java
@@ -1,6 +1,5 @@
 package com.iaraapi.repository;
 
-import com.iaraapi.model.database.User;
 import com.iaraapi.model.database.UserPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.query.Procedure;
